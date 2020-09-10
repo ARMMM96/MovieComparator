76d7b8f0926fac4d14968ef4782fa8f6c9970630
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2020-09-10 19:20:21 +0200

    Moving HTML Generation and Repairing References

--- index.js
+++ index.js
@@ -11,19 +11,36 @@ const fetchData = async (searchTerm) => {
   return response.data.Search;
 };
 
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+<label> <b> Search For a Movie</b></label>
+<input class="input" type="text" />
+<div class="dropdown">
+  <div class="dropdown-menu">
+    <div class="dropdown-content results">
+ 
+    </div>
+  </div>
+</div>
+<div id="target"></div>
+`;
+
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
 
 const onInput = async (e) => {
   const movies = await fetchData(e.target.value);
-
+  dropdown.classList.add('is-active');
   for (let movie of movies) {
-    const div = document.createElement('div');
-
-    div.innerHTML = `
+    const movieItem = document.createElement('a');
+    movieItem.classList.add('dropdown-item');
+    movieItem.innerHTML = `
       <img src="${movie.Poster}" />
-      <h1>${movie.title} </h1>
+      <h5>${movie.Title} </h5>
     `;
-    document.querySelector('#target').appendChild(div);
+    resultsWrapper.appendChild(movieItem);
+    console.log(movie);
   }
 };
 input.addEventListener('input', debounce(onInput, 500));
