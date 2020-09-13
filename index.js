404ccfd17e758d917bd77e3df0b08fd10b2eae29
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2020-09-13 20:16:06 +0200

    Showing Two Summaries

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ const autocompleteConfig = {
   
     `;
   },
-  onOptionSelect(movie) {
-    onMovieSelect(movie);
-  },
+
   inputValue(movie) {
     return movie.Title;
   },
@@ -29,13 +27,21 @@ const autocompleteConfig = {
 createAutoComplete({
   ...autocompleteConfig,
   root: document.querySelector('#left-autocomplete'),
+  onOptionSelect(movie) {
+    document.querySelector('.tutorial').classList.add('is-hidden');
+    onMovieSelect(movie, document.querySelector('.left-summary'));
+  },
 });
 createAutoComplete({
   ...autocompleteConfig,
   root: document.querySelector('#right-autocomplete'),
+  onOptionSelect(movie) {
+    document.querySelector('.tutorial').classList.add('is-hidden');
+    onMovieSelect(movie, document.querySelector('.right-summary'));
+  },
 });
 
-const onMovieSelect = async (movie) => {
+const onMovieSelect = async (movie, summaryElement) => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
       apikey: 'fec063bf',
@@ -43,7 +49,7 @@ const onMovieSelect = async (movie) => {
     },
   });
   console.log(response.data);
-  document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+  summaryElement.innerHTML = movieTemplate(response.data);
 };
 
 const movieTemplate = (movieDetail) => {
