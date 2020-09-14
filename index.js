e287622d5b8daa63fcd6e66e60ce99752c0e3c90
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2020-09-14 21:09:43 +0200

    Applying Parsed Properties

--- index.js
+++ index.js
@@ -76,9 +76,8 @@ const movieTemplate = (movieDetail) => {
       return prev + value;
     }
   }, 0);
-  console.log(awards);
   return `
-    <article class='media'>
+    <article data-value=${awards} class='media'>
       <figure class='media-left'>
         <p class="image">
           <img src="${movieDetail.Poster}" />
@@ -96,19 +95,19 @@ const movieTemplate = (movieDetail) => {
       <p class="title"> ${movieDetail.Awards} </p>
       <p class="subtitle">Awards </p>
     </article> 
-    <article class="notification is-primary">
+    <article data-value=${dollars} class="notification is-primary">
       <p class="title"> ${movieDetail.BoxOffice} </p>
       <p class="subtitle">Box Office </p>
     </article> 
-    <article class="notification is-primary">
+    <article data-value=${metascore} class="notification is-primary">
       <p class="title"> ${movieDetail.Metascore} </p>
       <p class="subtitle">Metascore</p>
     </article> 
-    <article class="notification is-primary">
+    <article data-value=${imdbRating} class="notification is-primary">
       <p class="title"> ${movieDetail.imdbRating} </p>
       <p class="subtitle">IMDB Rating </p>
     </article> 
-    <article class="notification is-primary">
+    <article data-value=${imdbVotes} class="notification is-primary">
       <p class="title"> ${movieDetail.imdbVotes} </p>
       <p class="subtitle">IMDB Votes </p>
     </article> 
